373ba95e18bc8a527ddfe0650193ee04e0ed4c00
dabaosod011 <dabaosod011@gmail.com>
2014-09-19 21:28:16 +0800

    add permutations2

--- src/leetcode/math_permutations2.cpp
+++ src/leetcode/math_permutations2.cpp
@@ -1,16 +1,16 @@
 #include "lc_header.h"
 
 /**
- *	https://oj.leetcode.com/problems/permutations/
+ *	https://oj.leetcode.com/problems/permutations-ii/
  */
 
-namespace permutations
+namespace permutations2
 {
 	class Solution 
 	{
 		vector<vector<int> > result; 
 	public:
-		vector<vector<int> > permute(vector<int> &num)
+		vector<vector<int> > permuteUnique(vector<int> &num)
 		{
 			generate(num, 0, num.size());  
 			return result;			
@@ -23,25 +23,35 @@ namespace permutations
 				result.push_back(num);
 				return;
 			}
-			
+          
 			for(int j = i; j < total; j++)
-			{ 
-				std::swap(num[i], num[j]);  
-				generate(num, i+1, total);  
-				std::swap(num[i], num[j]);  
+			{  
+				bool flag = true;
+				for (int k = i; k < j; k++)
+				{	
+					if(num[k] == num[j])
+						flag = false;
+				}
+
+				if(flag)
+				{
+					std::swap(num[i], num[j]);  
+					generate(num, i+1, total);  
+					std::swap(num[i], num[j]);  
+				}
 			}  
 		}  
 
 	};
 };
 
-BOOST_AUTO_TEST_SUITE( permutations_test )
-	boost::shared_ptr<permutations::Solution> solution_(new permutations::Solution());
+BOOST_AUTO_TEST_SUITE( permutations2_test )
+	boost::shared_ptr<permutations2::Solution> solution_(new permutations2::Solution());
 
 	BOOST_AUTO_TEST_CASE( case1 ) 	
 	{
-		std::cout << "running permutations_test case1..." <<std::endl;
-		vector<int> numbers = boost::assign::list_of(1)(2)(3);
+		std::cout << "running permutations2_test case1..." <<std::endl;
+		vector<int> numbers = boost::assign::list_of(1)(2)(2);
 		vector<vector<int> > result = solution_->permute(numbers);
 
 		for (auto it1 = result.begin(); it1 != result.end(); it1++)
